88201741d7246726762b39d64e4b35434a68516b
laurenfb <laurenfriesbrink@gmail.com>
2017-01-03 15:32:24 -0800

    rough draft of url, parse, toJSON for models

--- src/app/models/project.js
+++ src/app/models/project.js
@@ -1,7 +1,16 @@
 import Backbone from 'backbone';
 
 const Project = Backbone.Model.extend({
+  url: '',
+  parse: function(){},
+  toJSON: function(){},
 
+  initialize: function(options){
+    this.set("clusterID", options.clusterID);
+    this.set("photoURL", options.photoURL);
+    this.set("timeInDays", options.timeInDays)
+
+  }
 });
 
 export default Project;
--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,6 +1,15 @@
 import Backbone from 'backbone';
 
 const User = Backbone.Model.extend({
+  url: 'this is the url for my flask API',
+  parse: function(){
+    // code here to pull user info from the flask API
+  },
+  toJSON: function(){
+    // code here to set up a nice JSON to return. that returned JSON will then be saved by appview
+    // var userToSave = {jsonstuff}
+    // return userToSave
+  }
 
 });
 
