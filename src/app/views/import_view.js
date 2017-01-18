f833be66a6ee751ca076e14d5d241c6561a4fac7
laurenfb <laurenfriesbrink@gmail.com>
2017-01-18 14:05:02 -0800

    it's not rendering beautifully, but it's rendering REAL. LIVE. DATAAAAAAAA

--- src/app/views/import_view.js
+++ src/app/views/import_view.js
@@ -4,37 +4,37 @@ import $ from 'jquery';
 import Project from '../models/project'
 import Cluster from '../collections/cluster'
 import ClusterView from './cluster_view'
-
-var clusters = [{ "name": "sweaters", "projects": [
-  {clusterID: 1,
-    photoURL: "http://placebeyonce.com/250-250",
-    timeInDays: 28,
-    name: "blue sweater"},
-  {clusterID: 1,
-    photoURL: "http://placebeyonce.com/250-250",
-    timeInDays: 12,
-    name: "husband sweater"},
-  {clusterID: 1,
-    photoURL: "http://placebeyonce.com/250-250",
-    timeInDays: 100,
-    name: "customfit sweater"},
-  {clusterID: 1,
-    photoURL: "http://placebeyonce.com/250-250",
-    timeInDays: 29,
-    name: "cat sweater"}
-  ]},
-
-  {"name": "socks", "projects": [
-    {clusterID: 2,
-      photoURL: "http://placebeyonce.com/250-250",
-      timeInDays: 14,
-      name: "ctb socks"},
-    {clusterID: 2,
-      photoURL: "http://placebeyonce.com/250-250",
-      timeInDays: 12,
-      name: "socks for me"}
-  ]}
-]
+//
+// var clusters = [{ "name": "sweaters", "projects": [
+//   {clusterID: 1,
+//     photoURL: "http://placebeyonce.com/250-250",
+//     timeInDays: 28,
+//     name: "blue sweater"},
+//   {clusterID: 1,
+//     photoURL: "http://placebeyonce.com/250-250",
+//     timeInDays: 12,
+//     name: "husband sweater"},
+//   {clusterID: 1,
+//     photoURL: "http://placebeyonce.com/250-250",
+//     timeInDays: 100,
+//     name: "customfit sweater"},
+//   {clusterID: 1,
+//     photoURL: "http://placebeyonce.com/250-250",
+//     timeInDays: 29,
+//     name: "cat sweater"}
+//   ]},
+//
+//   {"name": "socks", "projects": [
+//     {clusterID: 2,
+//       photoURL: "http://placebeyonce.com/250-250",
+//       timeInDays: 14,
+//       name: "ctb socks"},
+//     {clusterID: 2,
+//       photoURL: "http://placebeyonce.com/250-250",
+//       timeInDays: 12,
+//       name: "socks for me"}
+//   ]}
+// ]
 
 const ImportView = Backbone.View.extend({
   initialize: function(){
@@ -53,24 +53,43 @@ const ImportView = Backbone.View.extend({
   },
 
   renderClusters: function() {
-    clusters.forEach( function(clus){
-      // grabbing this so i can make a special id in each div.
-      let clusID = clus["projects"][0]["clusterID"]
-      let cluster =  new Cluster(null, {name: clus["name"], id: clusID});
-      let clusterView = new ClusterView({
-        model: cluster
-      });
-      for (var i = 0; i < clus["projects"].length; i++) {
-        let project = new Project(clus["projects"][i])
-        cluster.add(project)
-      }
-      // add cluster to instance var
-      this.clusterCollections.push(cluster)
-      // render cluster and append it to 'main'
-      this.$el.append(clusterView.render().$el);
+    // using these vars because the .fetch.done() doesn't let me use a foreach as nicely as an anon fx, so 'this' is not available inside.
+    var clusCollect = this.clusterCollections
+    var element = this.$el
+    var self = this
+    this.model.fetch().done(
+      function(response){
+      console.log(response["clusters"].length)
+      for (var i = 0; i < response["clusters"].length; i++) {
+        let clus = response["clusters"][i];
+        // strangely, i am getting some empty clusters back which should not be possible but oh well!
+        if (clus["projects"].length != 0) {
+          let clusID = clus["projects"][0]["cluster_id"]
+          let cluster =  new Cluster(null,
+                                    {name: clus["name"],
+                                    id: clusID});
+          console.log(cluster)
+          let clusterView = new ClusterView({
+            model: cluster
+          });
 
-    }, this);
-  },
+          self.putProjectsInClusters(cluster, clus["projects"])
+          // for (var i = 0; i < clus["projects"].length; i++) {
+          //   let project = new Project(clus["projects"][i])
+          //   cluster.add(project)
+          // }
+          console.log(cluster)
+          // add cluster to instance var
+          clusCollect.push(cluster)
+          // render cluster and append it to 'main'
+          element.append(clusterView.render().$el);
+
+        }
+
+
+      }
+    }
+  )},
 
   events: {
     'click .btn-import-save': 'sendClusters'
@@ -79,7 +98,34 @@ const ImportView = Backbone.View.extend({
   sendClusters: function(){
     $('.btn-import-save').hide()
     this.trigger('clustersIncoming', this.clusterCollections)
+  },
+
+  putProjectsInClusters: function(cluster, listOfProjects) {
+    for (var i = 0; i < listOfProjects.length; i++) {
+      let project = new Project(listOfProjects[i])
+      cluster.add(project)
+    }
+    return cluster
   }
 });
 
 export default ImportView;
+
+
+// clusters.forEach( function(clus){
+//   // grabbing this so i can make a special id in each div.
+//   let clusID = clus["projects"][0]["clusterID"]
+//   let cluster =  new Cluster(null, {name: clus["name"], id: clusID});
+//   let clusterView = new ClusterView({
+//     model: cluster
+//   });
+//   for (var i = 0; i < clus["projects"].length; i++) {
+//     let project = new Project(clus["projects"][i])
+//     cluster.add(project)
+//   }
+//   // add cluster to instance var
+//   this.clusterCollections.push(cluster)
+//   // render cluster and append it to 'main'
+//   this.$el.append(clusterView.render().$el);
+//
+// }, this);
