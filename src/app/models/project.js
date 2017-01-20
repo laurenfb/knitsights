b0f48132c8130f052c857b1360ed69de9f8b2c62
laurenfb <laurenfriesbrink@gmail.com>
2017-01-19 17:25:06 -0800

    choppin' off long names like ya do

--- src/app/models/project.js
+++ src/app/models/project.js
@@ -3,7 +3,11 @@ import Backbone from 'backbone';
 const Project = Backbone.Model.extend({
 
   initialize: function(options){
-    this.set("name", options.name.toLowerCase())
+    var name = options.name.toLowerCase();
+    if (name.length >= 17) {
+      name = name.slice(0,13) + "..."
+    }
+    this.set("name", name)
     this.set("clusterID", options.cluster_id);
     this.set("photoURL", options.photo_url);
     this.set("timeInDays", options.time_in_days)
