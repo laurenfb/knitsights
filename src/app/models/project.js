5c6a6ce6153a33322d9bf878fdc4b5f5861035a8
laurenfb <laurenfriesbrink@gmail.com>
2017-01-20 11:33:05 -0800

    changed up name truncation

--- src/app/models/project.js
+++ src/app/models/project.js
@@ -5,9 +5,12 @@ const Project = Backbone.Model.extend({
   initialize: function(options){
     var name = options.name.toLowerCase();
     if (name.length >= 17) {
-      name = name.slice(0,13) + "..."
+      shortName = shortname.slice(0,13) + "...";
+    } else {
+      shortName = name
     }
     this.set("name", name)
+    this.set("shortName", shortName)
     this.set("clusterID", options.cluster_id);
     this.set("photoURL", options.photo_url);
     this.set("timeInDays", options.time_in_days)
