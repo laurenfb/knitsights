50ae71ddb63bfa83027cde6aef99752089817069
laurenfb <laurenfriesbrink@gmail.com>
2017-01-20 13:30:30 -0800

    merge conflict

--- src/app/models/project.js
+++ src/app/models/project.js
@@ -4,13 +4,13 @@ const Project = Backbone.Model.extend({
 
   initialize: function(options){
     var name = options.name.toLowerCase();
-    if (name.length >= 17) {
-      shortName = shortname.slice(0,13) + "...";
-    } else {
-      shortName = name
-    }
+    // if (name.length >= 17) {
+    //   shortName = shortname.slice(0,13) + "...";
+    // } else {
+    //   shortName = name
+    // }
     this.set("name", name)
-    this.set("shortName", shortName)
+    this.set("shortName", "shortername")
     this.set("clusterID", options.cluster_id);
     this.set("photoURL", options.photo_url);
     this.set("timeInDays", options.time_in_days)
