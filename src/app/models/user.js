9a5c950100d14dff49a3e301f69a9d5bc1849612
laurenfb <laurenfriesbrink@gmail.com>
2017-01-20 11:29:41 -0800

    change url

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,7 +1,7 @@
 import Backbone from 'backbone';
 
 const User = Backbone.Model.extend({
-  url:  'https://localhost:8000/api/get_projects/laureneliz',
+  url:  'https://knitsights-backend.herokuapp.com/api/get_projects/laureneliz',
 
   parse: function(){
 
