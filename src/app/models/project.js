dfdb7a40f53a7ba9eb853c51c6ebec072248ce1d
laurenfb <laurenfriesbrink@gmail.com>
2017-01-21 17:31:33 -0800

    WHY DOES MY SHORTNAME FX KEEP GOING AWAY?????

--- src/app/models/project.js
+++ src/app/models/project.js
@@ -7,9 +7,10 @@ const Project = Backbone.Model.extend({
     if (name.length >= 17) {
       var shortName = name.slice(0,13) + "...";
     } else {
-      var shortName = name;
+      var shortName = name
     }
     this.set("name", name)
+    this.set("shortName", shortName)
     this.set("clusterID", options.cluster_id);
     this.set("photoURL", options.photo_url);
     this.set("timeInDays", options.time_in_days)
