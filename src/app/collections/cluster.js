e9645b61443b742963d0a79008f7da66a5f56b08
laurenfb <laurenfriesbrink@gmail.com>
2017-01-25 10:42:09 -0800

    add longest and shortest days to the thumbnail view

--- src/app/collections/cluster.js
+++ src/app/collections/cluster.js
@@ -14,20 +14,50 @@ const Cluster = Backbone.Collection.extend({
     // console.log("we've gotten to calcAverageDays")
     let total = 0;
     for (var i = 0; i < this.models.length; i++) {
-      total += this.models[i].get("timeInDays")
+      total += this.models[i].get("timeInDays");
     }
-    let avg = parseInt(total/this.models.length)
-    return avg;
+    let avg = parseInt(total/this.models.length);
+    if (avg === 1) {
+      return "1 day";
+    } else {
+      return avg.toString() + " days";
+    }
   },
 
   getRandomPhoto: function(){
     let min = 0;
-    let max = this.models.length
+    let max = this.models.length;
     // taken from the internet, gets random number between min (incl) and max (excl)
     // exclsive bc if the length is 4 the highest index we'll want is 3.
     let random = Math.floor(Math.random() * (max - min)) + min;
     let photo = this.models[random].get("photoURL");
     return photo;
+  },
+
+  getShortestProj: function(){
+    let arrDays = [];
+    for (var i = 0; i < this.models.length; i++) {
+      arrDays.push(this.models[i].get("timeInDays"));
+    }
+    let min = Math.min(...arrDays);
+    if (min === 1) {
+      return "1 day";
+    } else {
+      return min.toString() + " days";
+    }
+  },
+
+  getLongestProj: function(){
+    let arrDays = [];
+    for (var i = 0; i < this.models.length; i++) {
+      arrDays.push(this.models[i].get("timeInDays"));
+    }
+    let max = Math.max(...arrDays);
+    if (max === 1) {
+      return "1 day";
+    } else {
+      return max.toString() + " days";
+    }
   }
 
 
--- src/app/views/thumbnail_view.js
+++ src/app/views/thumbnail_view.js
@@ -13,14 +13,13 @@ const ThumbnailView = Backbone.View.extend({
   render: function() {
     this.$el.append(this.thumbnailT(
       {model: this.model,
-        photoURL: this.getPhoto(),
+        photoURL: this.model.getRandomPhoto(),
         avgDays: this.model.calcAverageDays(),
-        name: this.model.name}))
+        name: this.model.name,
+        shortest: this.model.getShortestProj(),
+        longest: this.model.getLongestProj()
+      }))
     return this;
-  },
-
-  getPhoto: function(){
-    return this.model.getRandomPhoto();
   }
 });
 
